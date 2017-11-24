2f8068904e330b4b82a73151742ed24240627fe8
Jakob Thrane <jathr@fotonik.dtu.dk>
2017-11-24 10:54:03 +0100

    Added function for loading raman data.

---
+++ load_data.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def load_raman_map():
+
+
+    meas = pd.read_csv('data/raman_40x40_wavenumbers500_hotspots10_0dB_withinteractions.csv', header=None)
+    print(meas.shape)
+    substance1_map = meas.iloc[0,:]
+    substance1_r = substance1_map.values.reshape(1600,500)
+
+    substance2_map = meas.iloc[1,:]
+    substance2_r = substance2_map.values.reshape(1600,500)
+
+    mix_map = meas.iloc[2,:]
+    mix_r = mix_map.values.reshape(1600,500)
+
+    plt.figure(figsize=(12,12))
+    plt.subplot(3,1,1)
+    plt.plot(substance1_r)
+
+    plt.subplot(3,1,2)
+    plt.plot(substance2_r)
+    plt.subplot(3,1,3)
+    plt.plot(mix_r)
+
+    plt.show(block=False)
+
+    return substance1_r,substance2_r,mix_r
+
+
+if __name__=="__main__":
+    substance1_r,substance2_r,mix_r = load_raman_map()
